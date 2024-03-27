9dcaeee398ba0f1c14a1deba79a89ae7ef42912b
Gabriel Carmo <gabrielcarmo1602@gmail.com>
2024-03-27 11:12:01 -0300

    ajeitando estetica do codigo do ex3

--- ex2.py
+++ ex2.py
@@ -1,5 +1,6 @@
 soma = 0 
 cont = 0
+
 while cont < 10:
     nota = float(input("Digite uma nota: "))
     if nota > 10 or nota < 0:
@@ -7,5 +8,6 @@ while cont < 10:
     else:
         soma = soma + nota
         cont += 1
+        
 media = soma / cont
 print(f"Sua média foi: {media}")
