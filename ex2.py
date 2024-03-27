4453075c1f743feaf775826ccc2691d2c356c536
Gabriel Carmo <gabrielcarmo1602@gmail.com>
2024-03-27 10:48:01 -0300

    ex2 foi atualizado para avisar e nao reconhecer notas invalidas

--- ex2.py
+++ ex2.py
@@ -1,8 +1,11 @@
 soma = 0 
 cont = 0
 while cont < 10:
-    nota =float(input("Digite uma nota: "))
-    soma = soma + nota
-    cont += 1
+    nota = float(input("Digite uma nota: "))
+    if nota > 10 or nota < 0:
+        print("Nota invalida, informe novamente...")
+    else:
+        soma = soma + nota
+        cont += 1
 media = soma / cont
 print(f"Sua média foi: {media}")
